4b8bd295c7748592dd52d9c7c9267be274ea5b69
Guilherme Raduenz <graduenz@gmail.com>
2024-06-21 07:30:02 -0300

    chore: added first tests

---
+++ tests/MyDDNS.Core.Tests/IP/HttpIpAddressFetchStrategyTests.cs
@@ -0,0 +1,108 @@
+﻿using System.Net;
+using FluentAssertions;
+using Moq;
+using Moq.Protected;
+using MyDDNS.Core.IP;
+
+namespace MyDDNS.Core.Tests.IP;
+
+public class HttpIpAddressFetchStrategyTests
+{
+    private const string TestIp = "10.10.10.10";
+
+    public static object[][] HttpIpAddressFetchStrategy_WhenNullCtorParams_Throws_Data =>
+    [
+        [CreateHttpClientFactoryMock().Object, null!],
+        [null!, GetTestIpProviders()]
+    ];
+
+    [Theory]
+    [MemberData(nameof(HttpIpAddressFetchStrategy_WhenNullCtorParams_Throws_Data))]
+    public void HttpIpAddressFetchStrategy_WhenNullCtorParams_Throws(IHttpClientFactory httpClientFactory,
+        IEnumerable<Uri> ipProviders)
+    {
+        // Act
+        var act = () => new HttpIpAddressFetchStrategy(httpClientFactory, ipProviders);
+
+        // Assert
+        act.Should().Throw<ArgumentNullException>();
+    }
+
+    [Fact]
+    public void HttpIpAddressFetchStrategy_WhenEmptyIpProviders_Throws()
+    {
+        // Act
+        var act = () => new HttpIpAddressFetchStrategy(CreateHttpClientFactoryMock().Object, new List<Uri>());
+
+        // Assert
+        act.Should().Throw<ArgumentException>()
+            .WithParameterName("ipProviders")
+            .And.Message.Should().StartWith("At least one IP provider must be specified.");
+    }
+
+    [Theory]
+    [InlineData(null)]
+    [InlineData("")]
+    [InlineData("some kind of error")]
+    public async Task GetIpAddressAsync_WhenIpIsInvalid_ReturnsIPNone(string responseString)
+    {
+        // Arrange
+        var strategy =
+            new HttpIpAddressFetchStrategy(CreateHttpClientFactoryMock(responseString).Object, GetTestIpProviders());
+
+        // Act
+        var ip = await strategy.GetIpAddressAsync();
+
+        // Asset
+        ip.Should().Be(IPAddress.None);
+    }
+
+    [Fact]
+    public async Task GetIpAddressAsync_Success()
+    {
+        // Arrange
+        var strategy = new HttpIpAddressFetchStrategy(CreateHttpClientFactoryMock().Object, GetTestIpProviders());
+
+        // Act
+        var ip = await strategy.GetIpAddressAsync();
+
+        // Asset
+        ip.Should().Be(IPAddress.None);
+    }
+
+    private static List<Uri> GetTestIpProviders() =>
+    [
+        new Uri("https://provider1.com"),
+        new Uri("https://provider2.com")
+    ];
+
+    private static Mock<IHttpClientFactory> CreateHttpClientFactoryMock(string? responseString = TestIp)
+    {
+        var responseMessage = new HttpResponseMessage(HttpStatusCode.OK)
+        {
+            Content = responseString == null ? null : new StringContent(responseString)
+        };
+
+        var httpMessageHandlerMock = new Mock<HttpMessageHandler>();
+        httpMessageHandlerMock
+            .Protected()
+            .Setup<Task<HttpResponseMessage>>(
+                "SendAsync",
+                ItExpr.IsAny<HttpRequestMessage>(),
+                ItExpr.IsAny<CancellationToken>()
+            )
+            .Returns(Task.FromResult(responseMessage))
+            .Verifiable();
+
+        var httpClient = new HttpClient(httpMessageHandlerMock.Object)
+        {
+            BaseAddress = new Uri("https://127.0.0.1/")
+        };
+
+        var httpClientFactoryMock = new Mock<IHttpClientFactory>();
+
+        httpClientFactoryMock.Setup(m => m.CreateClient(It.IsAny<string>())).Returns(httpClient);
+
+        return httpClientFactoryMock;
+    }
+}
